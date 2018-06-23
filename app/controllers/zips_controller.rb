1e89aeb7efa3acf7f36e56e7f78d5ca9b8732457
apoc64 <sschwedt@gmail.com>
2018-06-23 12:56:03 -0600

    adds google maps

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -1,5 +1,8 @@
 class ZipsController < ApplicationController
+
   def show
     @zip = Zip.find(params[:id])
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@zip.id}&key=#{Rails.application.credentials.google[:api_key]}")
+    @coords = response["results"][0]["geometry"]["bounds"]["northeast"]
   end
 end
