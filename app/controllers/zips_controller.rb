36116e604ae10ba8d34dc9383a4bb0c8270fe77c
apoc64 <sschwedt@gmail.com>
2018-07-01 18:49:52 -0600

    deleted extra whitespace

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -3,7 +3,6 @@ class ZipsController < ApplicationController
     @high_incomes = Zip.select('zips.id, zips.a00100, zips.n1, (zips.a00100 / zips.n1) AS income').order('income DESC').limit(10)
     @low_incomes = Zip.select('zips.id, zips.a00100, zips.n1, (zips.a00100 / zips.n1) AS income').order('income ASC').limit(10)
     @highest_pops = Zip.select('id, n1').order('n1 DESC').limit(10)
-    # binding.pry
   end
 
   def show
