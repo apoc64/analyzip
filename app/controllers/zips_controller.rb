8c739b96b2321191a15c80941c9dfa2b59b81846
apoc64 <sschwedt@gmail.com>
2018-07-08 16:24:47 -0600

    tests nav bar

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -8,6 +8,6 @@ class ZipsController < ApplicationController
   def show
     @zip = Zip.find(params[:id])
     response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@zip.id}&key=#{Rails.application.credentials.google[:api_key]}")
-    @coords = response["results"][0]["geometry"]["bounds"]["northeast"]
+    @coords = response["results"][0]["geometry"]["location"]
   end
 end
--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -4,8 +4,31 @@ describe 'user visits home page' do
   it 'has a welcome message and links' do
     visit '/'
 
-    expect(page).to have_content("Welcome to Zip Code Analyzer")
+    expect(page).to have_content("Welcome to AnalyZip")
     expect(page).to have_link(href: states_path)
     expect(page).to have_link(href: zips_path)
+  end
+
+  it 'can navigate to nav links' do
+    visit '/'
+
+    within('.nav-wrapper') do
+      click_on 'States'
+      expect(current_path).to eq(states_path)
+
+      click_on 'Zip Codes'
+      expect(current_path).to eq(zips_path)
+
+      click_on 'AnalyZip'
+      expect(current_path).to eq(root_path)
+    end
+
+    within('.sidenav') do
+      click_on 'States'
+      expect(current_path).to eq(states_path)
+
+      click_on 'Zip Codes'
+      expect(current_path).to eq(zips_path)
+    end
   end
 end
