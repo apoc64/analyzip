bd46657877c9c41c49c7cbe631f97aa85a7c0af8
apoc64 <sschwedt@gmail.com>
2018-07-08 16:42:37 -0600

    adds zip presenter

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -1,13 +1,12 @@
 class ZipsController < ApplicationController
   def index
-    @high_incomes = Zip.select('zips.id, zips.a00100, zips.n1, (zips.a00100 / zips.n1) AS income').order('income DESC').limit(10)
-    @low_incomes = Zip.select('zips.id, zips.a00100, zips.n1, (zips.a00100 / zips.n1) AS income').order('income ASC').limit(10)
-    @highest_pops = Zip.select('id, n1').order('n1 DESC').limit(10)
+    @zips = ZipsIndexPresenter.new
   end
 
   def show
-    @zip = Zip.find(params[:id])
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@zip.id}&key=#{Rails.application.credentials.google[:api_key]}")
-    @coords = response["results"][0]["geometry"]["location"]
+    @zip = ZipPresenter.new(params[:id])
+    # @zip = Zip.find(params[:id])
+    # response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@zip.id}&key=#{Rails.application.credentials.google[:api_key]}")
+    # @coords = response["results"][0]["geometry"]["location"]
   end
 end
