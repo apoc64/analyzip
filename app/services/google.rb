231c676a675b6de699cc010d2d2d66c1c6ab9e50
apoc64 <sschwedt@gmail.com>
2018-07-19 10:58:16 -0600

    prepares project for aws deploy

--- app/services/google.rb
+++ app/services/google.rb
@@ -2,7 +2,16 @@ class Google
   def zip_coords(zip_code)
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{zip_code}&key=#{Rails.application.credentials.google[:api_key]}"
     response = HTTParty.get(url)
-    # binding.pry
-    response["results"][0]["geometry"]["location"]
+    if response["status"] != "ZERO_RESULTS"
+      # where ["types"].includes?"administrative_area_level_2"
+      county = response["results"][0]["address_components"][2]["short_name"]
+      # binding.pry
+      response["results"][0]["geometry"]["location"]
+      # ne se - size - viewport
+    else
+      # 81041
+      puts "no results for #{zip_code}"
+      {"lat" => 0, "lng" => 0}
+    end
   end
 end
