fd32afbe4104e67182e8407e199e16b622f4c3c8
apoc64 <sschwedt@gmail.com>
2018-07-19 13:01:17 -0600

    guard statements on maps api

--- app/services/google.rb
+++ app/services/google.rb
@@ -1,7 +1,13 @@
 class Google
   def zip_coords(zip_code)
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{zip_code}&key=#{Rails.application.credentials.google[:api_key]}"
-    response = HTTParty.get(url)
+    if Rails.application.credentials.google[:api_key]
+      url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{zip_code}&key=#{Rails.application.credentials.google[:api_key]}"
+      response = HTTParty.get(url)
+    else
+      response = {"status" => "ZERO_RESULTS"}
+    end
+
+
     if response["status"] != "ZERO_RESULTS"
       # where ["types"].includes?"administrative_area_level_2"
       county = response["results"][0]["address_components"][2]["short_name"]
