4f2e1842ac70d3a9f9e7bf64f5f51bf778cf1a35
apoc64 <sschwedt@gmail.com>
2018-07-27 08:43:44 -0600

    tested for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+  end
+
+  # describe 'relationships' do
+  #   it {should have_many(:zips) through...}
+  #   it {should have_many(:counties)}
+  # end
+end
