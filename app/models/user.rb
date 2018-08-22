0ca2ddae3240712a0989e6ab8c026501f905c351
apoc64 <sschwedt@gmail.com>
2018-08-22 11:55:21 -0600

    Removes additional unnecessary comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  # has_secure_password
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates :email, presence: true, uniqueness: true
