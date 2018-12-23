747b229535438c3fd011c53cfccadac8b80784b7
apoc64 <sschwedt@gmail.com>
2018-12-23 13:31:28 -0700

    Adds tests for code string method

--- spec/models/zip_spec.rb
+++ spec/models/zip_spec.rb
@@ -20,5 +20,19 @@ RSpec.describe Zip, type: :model do
       zip = create(:zip)
       expect(zip.percent_joint).to eq(((zip.mars2.to_f / zip.n1.to_f) * 100).to_i)
     end
+
+    describe 'code is 5 character number string' do
+      it 'returns a string' do
+        state = State.create(abbreviation: 'CO')
+        zip = Zip.create(id: 80202, n1: 5000, mars1: 2000, mars2: 2500, mars4: 1000, prep: 750, numdep: 3000, elderly: 750, a00100: 4000000, state: state)
+        expect(zip.code).to eq('80202')
+      end
+
+      it 'Adds a zero for 4 digit ids' do
+        state = State.create(abbreviation: 'CO')
+        zip = Zip.create(id: 8202, n1: 5000, mars1: 2000, mars2: 2500, mars4: 1000, prep: 750, numdep: 3000, elderly: 750, a00100: 4000000, state: state)
+        expect(zip.code).to eq('08202')
+      end
+    end
   end
 end
