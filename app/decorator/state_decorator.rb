e70f2ac809cb65507dee694f7941e778a24f5ef4
apoc64 <sschwedt@gmail.com>
2018-12-25 18:18:08 -0700

    Extracts methods to decorator superclass

--- app/decorator/state_decorator.rb
+++ app/decorator/state_decorator.rb
@@ -1,25 +1,6 @@
-class StateDecorator < SimpleDelegator
-  include ActionView::Helpers::NumberHelper
-  delegate :url_helpers, to: 'Rails.application.routes'
+class StateDecorator < Decorator
 
   def path
     url_helpers.state_path(self)
-  end
-
-  # Move to superclass, test exact dollars on zip, float SQL
-  def link_name
-    code
-  end
-
-  def set_currency
-    @value = number_to_currency((raw_value * 1000), precision: 0)
-  end
-
-  def set_delimiter
-    @value = number_with_delimiter(raw_value)
-  end
-
-  def value
-    @value || raw_value
   end
 end
--- app/decorator/zip_decorator.rb
+++ app/decorator/zip_decorator.rb
@@ -1,24 +1,7 @@
-class ZipDecorator < SimpleDelegator
-  include ActionView::Helpers::NumberHelper
-  delegate :url_helpers, to: 'Rails.application.routes'
-
-  def link_name
-    code
-  end
+class ZipDecorator < Decorator
 
   def path
     url_helpers.zip_path(self)
   end
 
-  def set_currency
-    @value = number_to_currency(raw_value * 1000)
-  end
-
-  def set_delimiter
-    @value = number_with_delimiter(raw_value)
-  end
-
-  def value
-    @value || raw_value
-  end
 end
